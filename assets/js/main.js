1a7d6cdcd88835cebc070f813a36c2a2e08bd5cd
jameshedaweng <heda.weng@gmail.com>
2015-10-24 16:36:19 +0200

    Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,41 @@ var MP = [];
 
 $(document).ready(function(){
   MP.mapInit();
-  simulateDownloadActivities();
+  MP.sliderInit();
+  //simulateDownloadActivities();
 });
 
 MP.mapInit = function(){
   L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXNoZWRhd2VuZyIsImEiOiJxNGxvT1h3In0.q1gGwhVt7lQ7Tji5NV2jUQ';
-  MP.map = L.mapbox.map('map', 'jameshedaweng.hig7dplk')
+  MP.map = L.mapbox.map('map', 'jameshedaweng.0b40c805')
     .setView([40.415363, -3.707398], 14);
   MP.map.scrollWheelZoom.disable();
+};
+
+MP.sliderInit = function(){
+  $("#slider-label").html(moment().format("HH:mm"));
+  $("#current-time").html(moment().format("HH:mm"));
+  $("#slider").slider({
+    animate: "fast",
+    max: 720,
+    min: -720,
+    step: 1,
+    value: 0,
+    slide: function(event, ui) {
+      var delay = function() {
+        var handleIndex = $(ui.handle).data('index.uiSliderHandle');
+        var label = "#slider-label";
+        var time = moment().add(ui.value, 'minute').format("HH:mm");
+        $(label).html(time).position({
+          my: 'center bottom',
+          at: 'center top',
+          of: ui.handle,
+          offset: "0, 15"
+        });
+        $("#current-time").html(time);
+      };
+      setTimeout(delay, 5);
+    }
+  });
+  $('#slider').draggable();
 };
