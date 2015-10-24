ac5b8adea864087c9d57763b0678ff6025943e8f
David Mañanes <dmananes@vexiza.com>
2015-10-24 13:56:37 +0200

    fix map error

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,5 @@ MP.mapInit = function(){
   L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXNoZWRhd2VuZyIsImEiOiJxNGxvT1h3In0.q1gGwhVt7lQ7Tji5NV2jUQ';
   MP.map = L.mapbox.map('map', 'jameshedaweng.hig7dplk')
     .setView([40.415363, -3.707398], 14);
-  map.scrollWheelZoom.disable();
+  MP.map.scrollWheelZoom.disable();
 };
