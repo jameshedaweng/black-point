dab4d8b1123de5f4fce334c6e7f8830961561de6
jameshedaweng <heda.weng@gmail.com>
2015-10-24 13:52:01 +0200

    Notifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,5 @@ MP.mapInit = function(){
   L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXNoZWRhd2VuZyIsImEiOiJxNGxvT1h3In0.q1gGwhVt7lQ7Tji5NV2jUQ';
   var map = L.mapbox.map('map', 'jameshedaweng.hig7dplk')
     .setView([40.415363, -3.707398], 14);
+  map.scrollWheelZoom.disable();
 };
