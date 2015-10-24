995c0a5032ecb02e04f11ab25759c82a757c1734
jameshedaweng <heda.weng@gmail.com>
2015-10-24 13:03:26 +0200

    Add map

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var MP = [];
+
+$(document).ready(function(){
+  MP.mapInit();
+});
+
+MP.mapInit = function(){
+  L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXNoZWRhd2VuZyIsImEiOiJxNGxvT1h3In0.q1gGwhVt7lQ7Tji5NV2jUQ';
+  var map = L.mapbox.map('map', 'jameshedaweng.hig7dplk')
+    .setView([40.415363, -3.707398], 14);
+};
